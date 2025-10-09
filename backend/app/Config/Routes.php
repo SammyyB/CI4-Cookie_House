78492d39068fc9e8a5b24b0a37eead269ea6c0fa
SammyyB <baccaysheryll@gmail.com>
2025-10-09 20:23:14 +0800

    feat(frontend/moodboardPage): created the routes, users, and moodboard_page.php

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,3 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Users::index');
 $routes->get('/', 'Users::login');
 $routes->get('/', 'Users::signup');
+$routes->get('/', 'Users::moodboard');
